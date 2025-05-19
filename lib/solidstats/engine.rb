8412e7c27d9f8e8b9dad192de6e16811bd57fa4f
MezbahAlam <mezbahalam26@gmail.com>
2025-05-19 13:13:26 +0600

    Update Solidstats version to 0.0.1 and add asset precompilation configuration for development

--- lib/solidstats/engine.rb
+++ lib/solidstats/engine.rb
@@ -7,5 +7,11 @@ module Solidstats
         raise "Solidstats can only be used in development mode."
       end
     end
+
+    initializer "solidstats.assets.precompile" do |app|
+      if Rails.env.development?
+        app.config.assets.precompile += %w( solidstats/application.css )
+      end
+    end
   end
 end
